4dff615de407faeabc8702fd4eeec57a32280394
Ayush Sharma <ayush.sharmaod21@gmail.com>
2024-06-15 01:15:51 +0530

    added cloudinary

---
+++ src/utils/cloudinaryFileHandling.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+//    fs = fileSystem
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath)return null
+
+        //upload the file
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+
+        //file uploaded
+        console.log("FILE HAS BEEN UPLOADED ON CLOUDINARY",response.url);
+        return response;
+
+    } catch (error) {
+        
+        // remove the file synchronously, saved on localpath if the upload failed
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+
+cloudinary.v2.uploader.upload("",{},(error,result) => {console.log(result)})
